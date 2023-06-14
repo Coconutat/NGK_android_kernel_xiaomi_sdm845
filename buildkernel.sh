bdd614ed97daa8fa41d2ef58a3c4706caf742a7c
Coconut <coconutat@protonmail.com>
2023-06-14 17:17:26 +0800

    Update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -12,14 +12,31 @@ export ARCH=arm64
 export SUBARCH=arm64
 # export DTC_EXT=dtc
 
-if [ ! -d "out" ];
-then
+if [ ! -d "out" ]; then
 	mkdir out
 fi
 
 date="$(date +%Y.%m.%d-%I:%M)"
 
-
 make ARCH=arm64 O=out CC=clang nogravity-dipper_ksu_defconfig
 # 定义编译线程数
 make ARCH=arm64 O=out $EV -j12 CC=clang 2>&1 | tee kernel_log-${date}.txt
+
+if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
+	echo "***Packing NGK-Dipper-KSU kernel...***"
+	cp out/arch/arm64/boot/Image.gz-dtb tools/NoGravityKernel/Image.gz-dtb
+	cp out/arch/arm64/boot/Image.gz-dtb Image.gz-dtb
+	cd tools/NoGravityKernel
+	zip -r9 NoGravityKernel-v3.2.1-KSU-${date}.zip * >/dev/null
+	cd ../..
+	mv tools/NoGravityKernel/NoGravityKernel-v3.2.1-KSU-${date}.zip NoGravityKernel-v3.2.1-KSU-${date}.zip
+	rm -rf tools/NoGravityKernel/Image.gz-dtb
+	echo " "
+	echo "***Sucessfully built NGK-Dipper-KSU kernel...***"
+	echo " "
+	exit 0
+else
+	echo " "
+	echo "***Failed!***"
+	exit 0
+fi
